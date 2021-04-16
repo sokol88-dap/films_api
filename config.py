16044067638c6889487731e4ee47050517300516
Yury Altukhov <sokol88.dap@gmail.com>
2021-04-16 19:00:24 +0700

    changes for 10th lesson

--- config.py
+++ config.py
@@ -12,3 +12,4 @@ class Config:
         str(BASE_DIR / "data" / "db.sqlite3")
     SQLALCHEMY_TRACK_MODIFICATION = False
     SECRET_KEY = 'you-will-never-know'
+    HEROKU_DATABASE_URI = 'postgres://vqfvdbikupidyn:e5d02493ddd4543f985348d07830478fdd5332fad24ae5516f386f7f3e5f9f41@ec2-34-242-89-204.eu-west-1.compute.amazonaws.com:5432/d66antqhhhtk6v'
